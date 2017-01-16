0662da57c05efc3abbf36e1507d9e27c4fbb3ac3
vceli <vickensi.delacruz@gmail.com>
2017-01-15 23:57:31 -0500

    formulario traducido

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.getElementById('form-signin-heading').innerHTML = "Por favor inicia sesión";
+document.getElementById('inputEmail').placeholder = "Correo Electrónico";
+document.getElementById('inputPassword').placeholder = "Contraseña";
+document.querySelector('.checkbox>label span').innerHTML="Recordar datos";
+document.getElementsByClassName('checkbox')[0].nextElementSibling.innerHTML = 'Iniciar Sesión';
+// document.getElementsByClassName('btn')[0].innerHTML = 'Iniciar Sesión';
+// document.getElementsByTagName('button').innerHTML = 'Iniciar Sesión';
+
+
+// var a = document.getElementById('form-signin-heading');
+// a.innerHTML = "reemplazo-texto";
